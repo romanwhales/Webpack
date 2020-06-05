4a198a55194457b2555c775a1184d364d2014b3f
Olawale Sunmonu-Adedeji <ssg315ass@yahoo.com>
2020-06-05 09:29:47 +0100

    Loading of Routes with objects

--- dist/bundle.671a9d69d8d2d9994a00.js
+++ dist/bundle.671a9d69d8d2d9994a00.js
@@ -1,4 +1,4 @@
-webpackJsonp([0],{
+webpackJsonp([1,2],{
 
 /***/ 116:
 /***/ function(module, exports, __webpack_require__) {
@@ -3256,6 +3256,8 @@ __webpack_require__(212);
 __webpack_require__(213);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+console.log('Hi!');
 
 var App = function App() {
   var store = (0, _redux.createStore)(_reducers2.default, {}, (0, _redux.applyMiddleware)(_reduxThunk2.default));
